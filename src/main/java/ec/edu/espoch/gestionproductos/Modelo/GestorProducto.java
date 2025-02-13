11ca0dacd39a779a33277562432c610e451be506
welinton-cool <welinton.sacon@espoch.edu.ec>
2025-02-13 14:55:49 -0500

    hola

--- src/main/java/ec/edu/espoch/gestionproductos/Modelo/GestorProducto.java
+++ src/main/java/ec/edu/espoch/gestionproductos/Modelo/GestorProducto.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ec.edu.espoch.gestionproductos.Modelo;
 
-/**
- *
- * @author HP
- */
+
 public class GestorProducto {
-    
+      private final Productos [] Productos;
+    private int contadorProductos;
+
+    public GestorProductos() {
+        Productos = new Productos[10];
+        contadorProductos = 0;
+         
+}
+ public void agregarTarea (int id, String nombre, String precio, boolean disponible) {
+    if (contadorProductos < Productos.length) {
+        Productos[contadorProductos] = new Productos (id, nombre, precio ); 
+        System.out.println("Productos añadida: " + id + " - " + nombre + " - " + precio + " - disponible: " + disponible);  // Espacios añadidos para mejor legibilidad
+        contadorProductos++;
+        } else {
+            System.out.println("No se pueden agregar mas tareas.");
+        }
+    }
+
+    public void isCompletada() {
+        System.out.println("Tareas Pendientes");
+        for (int i = 0; i < contadorProductos; i++) {
+            if (Productos[i].isDisponible()) {
+                System.out.println(i + "; " + Productos[i].getDescripcion());
+
+            }
+
+        }
+    }
+
+    public void isCompleta() {
+        System.out.println("Tareas Completadas");
+        for (int i = 0; i < contadorProductos; i++) {
+            if (!Productos[i].isDisponible()) {
+
+            } else {
+            }
+        }}
 }
--- src/main/java/ec/edu/espoch/gestionproductos/Modelo/Productos.java
+++ src/main/java/ec/edu/espoch/gestionproductos/Modelo/Productos.java
@@ -1,13 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ec.edu.espoch.gestionproductos.Modelo;
 
-/**
- *
- * @author HP
- */
+
 public class Productos {
+ private int id;
+    private String nombre;
+    private String precio;
+    private boolean disponible; 
+
+    public Productos(int id, String nombre, String precio, boolean disponible) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.disponible = disponible;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+    
     
 }
